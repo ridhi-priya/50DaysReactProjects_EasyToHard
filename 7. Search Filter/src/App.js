5d134ce173231396ebe2d5795193bba82646a602
Ridhi Priya <ridhi7681309@gmail.com>
2022-12-29 23:58:17 +0530

    Search filter

---
+++ 7. Search Filter/src/App.js
@@ -0,0 +1,35 @@
+import './App.css'
+import JSONDATA from './MOCK_DATA'
+import { useState } from 'react'
+
+function App() {
+  const [search, setsearch] = useState('')
+  return (
+    <div className='App'>
+      <input
+        type='text'
+        placeholder='Search...'
+        onChange={(event) => {
+          setsearch(event.target.value)
+        }}
+      />
+      {JSONDATA.filter((val) => {
+        if (search == '') {
+          return val
+        } else if (
+          val.first_name.toLowerCase().includes(search.toLowerCase())
+        ) {
+          return val
+        }
+      }).map((val, key) => {
+        return (
+          <div className='user' key={key}>
+            <p>{val.first_name}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App
